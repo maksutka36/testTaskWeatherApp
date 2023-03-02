b3c16c0f7c799cc7e5ef5ba28d3882da7a85a8ae
unknown <maksutka998@gmail.com>
2023-03-02 20:20:57 +0100

    commit

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Capital } from './capital.model';
 import { WeatherApi } from './weatherApi.model';
 import { Observable } from 'rxjs'
@@ -14,14 +14,15 @@ export class WeatherService {
   ) { }
 
   getWeather(capital: Capital, days: number): Observable<WeatherApi> {
-    let queryParams = new HttpParams();
-    queryParams = queryParams.append('latitude', capital.latitude);
-    queryParams = queryParams.append('longitude', capital.longitude);
-    queryParams = queryParams.append('daily', 'temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum');
-    queryParams = queryParams.append('timezone', 'auto');
-    queryParams = queryParams.append('start_date', new Date().toISOString().slice(0, 10));
-    queryParams = queryParams.append('end_date', new Date (new Date().setDate(new Date().getDate() + days - 1)).toISOString().slice(0, 10));
-
-    return this.http.get<WeatherApi>('https://api.open-meteo.com/v1/forecast', {params: queryParams});
+    return this.http.get<WeatherApi>('https://api.open-meteo.com/v1/forecast',{
+      params: {
+        latitude: capital.latitude,
+        longitude: capital.longitude,
+        daily: 'temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum',
+        timezone: 'auto',
+        start_date: new Date().toISOString().slice(0, 10),
+        end_date: new Date (new Date().setDate(new Date().getDate() + days - 1)).toISOString().slice(0, 10)
+      }
+  });
   }
 }
